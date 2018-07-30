f4fa59991139617182da34604f55c729f3cb6e47
Cédric Foellmi <cedric@onekilopars.ec>
2018-07-30 09:33:07 +0200

    Project setup thanks to https://nvie.com/posts/writing-a-cli-in-python-in-under-60-seconds/

---
+++ README.md
@@ -0,0 +1,21 @@
+# Arcsecond Python
+
+ CLI for arcsecond.io
+
+
+# Installation
+
+If you don't use `pipsi`, you're missing out.
+Here are [installation instructions](https://github.com/mitsuhiko/pipsi#readme).
+
+Simply run:
+
+    $ pipsi install .
+
+
+# Usage
+
+To use it:
+
+    $ arcsecond --help
+
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+ CLI for arcsecond.io
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='arcsecond',
+    version='0.1.0',
+    url='https://github.com/onekiloparsec/arcsecond-python',
+    license='MIT',
+    author='Cedric Foellmi',
+    author_email='cedric@arcsecond.io',
+    description=' CLI for arcsecond.io',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'arcsecond = arcsecond.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
