9fd76b36444067e2486d541e004e6039e4c48d31
Cédric Foellmi <cedric@f52.tech>
2018-08-15 13:10:25 +0200

    Adding config

---
+++ arcsecond/config.py
@@ -0,0 +1,33 @@
+import os
+from configparser import SafeConfigParser
+
+
+def config_file_path():
+    return os.path.expanduser('~/.arcsecond.ini')
+
+
+def config_file_exists():
+    path = config_file_path()
+    return os.path.exists(path) and os.path.isfile(path)
+
+
+def config_file_is_valid():
+    if not config_file_exists():
+        return False
+    config = SafeConfigParser()
+    config.read(config_file_path())
+    return config['main'].get('api_key')
+
+
+def config_file_save_api_key(api_key):
+    config = SafeConfigParser()
+    config.add_section('main')
+    config.set('main', 'api_key', api_key)
+    with open(config_file_path(), 'w') as f:
+        config.write(f)
+
+
+def config_file_read_api_key():
+    config = SafeConfigParser()
+    config.read(config_file_path())
+    return config['main'].get('api_key')
