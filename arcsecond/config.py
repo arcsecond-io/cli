b870a23fc0b0ca7dd8e41aa9d9683d3d0a03076e
Cédric Foellmi <cedric@f52.tech>
2018-08-15 19:12:39 +0200

    We need username too

--- arcsecond/config.py
+++ arcsecond/config.py
@@ -19,15 +19,24 @@ def config_file_is_valid():
     return config['main'].get('api_key')
 
 
-def config_file_save_api_key(api_key):
+def config_file_save_api_key(api_key, username):
     config = SafeConfigParser()
     config.add_section('main')
+    config.set('main', 'username', username)
     config.set('main', 'api_key', api_key)
     with open(config_file_path(), 'w') as f:
         config.write(f)
 
 
-def config_file_read_api_key():
+def config_file_read_key(key):
     config = SafeConfigParser()
     config.read(config_file_path())
-    return config['main'].get('api_key')
+    return config['main'].get(key)
+
+
+def config_file_read_api_key():
+    return config_file_read_key('api_key')
+
+
+def config_file_read_username():
+    return config_file_read_key('username')
