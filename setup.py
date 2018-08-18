8f398231b50af8a10adfcae85cd4039dbad01b14
Cédric Foellmi <cedric@onekilopars.ec>
2018-08-18 12:29:16 +0200

    making test pass

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 from arcsecond.cli import __version__
 
-dependencies = ['click', 'requests', 'pygments']
+dependencies = ['click', 'requests', 'pygments', 'configparser']
 
 setup(
     name='arcsecond',
