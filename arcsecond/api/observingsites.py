8c2b9e5c58bd574e5a759083574de31d0ad58eab
Cédric Foellmi <cedric@onekilopars.ec>
2018-09-16 18:52:02 +0200

    adding telescopes + using new check

--- arcsecond/api/observingsites.py
+++ arcsecond/api/observingsites.py
@@ -7,8 +7,20 @@ class ObservingSitesAPIEndPoint(APIEndPoint):
     def _list_url(self, name=None):
         return self._root_url() + '/observingsites/'
 
-    def _detail_url(self, uuid):
-        return self._root_url() + '/observingsites/' + uuid + '/'
+    def _detail_url(self, uuid_str):
+        self._check_uuid(uuid_str)
+        return self._root_url() + '/observingsites/' + uuid_str + '/'
+
+
+class TelescopesAPIEndPoint(APIEndPoint):
+    name = 'telescopes'
+
+    def _list_url(self, name=None):
+        return self._root_url() + '/telescopes/'
+
+    def _detail_url(self, uuid_str):
+        self._check_uuid(uuid_str)
+        return self._root_url() + '/telescopes/' + uuid_str + '/'
 
 
 class InstrumentsAPIEndPoint(APIEndPoint):
@@ -17,5 +29,6 @@ class InstrumentsAPIEndPoint(APIEndPoint):
     def _list_url(self, name=None):
         return self._root_url() + '/instruments/'
 
-    def _detail_url(self, uuid):
-        return self._root_url() + '/instruments/' + uuid + '/'
+    def _detail_url(self, uuid_str):
+        self._check_uuid(uuid_str)
+        return self._root_url() + '/instruments/' + uuid_str + '/'
