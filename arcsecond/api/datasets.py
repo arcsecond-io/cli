740163bbef4745cd57b42ad88d06d7238e605641
Cédric Foellmi <cedric@onekilopars.ec>
2018-10-08 22:43:35 +0200

    Preparing datasets

---
+++ arcsecond/api/datasets.py
@@ -0,0 +1,22 @@
+from .base import APIEndPoint
+
+
+class DatasetsAPIEndPoint(APIEndPoint):
+    name = 'datasets'
+
+    def _list_url(self, name=None):
+        return self._root_url() + '/datasets/'
+
+    def _detail_url(self, uuid_str):
+        self._check_uuid(uuid_str)
+        return self._root_url() + '/datasets/' + uuid_str + '/'
+
+
+class FITSFilesAPIEndPoint(APIEndPoint):
+    name = 'fitsfiles'
+
+    def _list_url(self, name=None):
+        return self._root_url() + '/fitsfiles/'
+
+    def _detail_url(self, pk_str):
+        return self._root_url() + '/fitsfiles/' + pk_str + '/'
