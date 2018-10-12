2d12081ae460c0315e5ab60a939f0baa833a61aa
Cédric Foellmi <cedric@onekilopars.ec>
2018-10-12 15:06:24 +0200

    Added login tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -26,3 +26,16 @@ def test_cli_version(runner):
     result = runner.invoke(cli.version)
     assert result.exit_code == 0 and not result.exception
     assert re.match('[0-9].[0-9].[0-9]', result.output)
+
+
+def test_login_unknown_credentials(runner):
+    result = runner.invoke(cli.login, ['-d'], input='dummy\ndummy')
+    assert result.exit_code == 0 and not result.exception
+    assert 'Unable to log in with provided credentials.' in result.output
+
+
+def test_login_invalid_parameters(runner):
+    for input in [' \n ', ' \ndummy', 'dummy\n ']:
+        result = runner.invoke(cli.login, ['-d'], input=input)
+        assert result.exit_code == 0 and not result.exception
+        assert 'non_field_errors' in result.output or 'This field may not be blank' in result.output
