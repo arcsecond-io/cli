de0d8959f1e669d2b0dfd66c822c8fff9a83b5f4
Cédric Foellmi <cedric@onekilopars.ec>
2018-10-12 15:41:55 +0200

    small refactor

--- arcsecond/api/_main.py
+++ arcsecond/api/_main.py
@@ -148,7 +148,7 @@ class ArcsecondAPI(object):
     @classmethod
     def login(cls, username, password, state=None):
         state = state or State()
-        result, error = AuthAPIEndPoint(state).authenticate(username, password)
+        result, error = AuthAPIEndPoint(state).login(username, password)
         if error:
             return ArcsecondAPI._echo_error(state, error)
         if result:
