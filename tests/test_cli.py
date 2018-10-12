8cf7242eab3941d786eb06c3139dea3c8611bb6f
Cédric Foellmi <cedric@onekilopars.ec>
2018-10-12 16:28:45 +0200

    No fixture for py2.7 incompatible with httpretty

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,26 +1,20 @@
 import re
-import pytest
 import httpretty
 from click.testing import CliRunner
 from arcsecond import cli, ArcsecondConnectionError
-from arcsecond.api._constants import (API_AUTH_PATH_LOGIN,
-                                      ARCSECOND_API_URL_DEV,
-                                      ARCSECOND_API_URL_PROD)
+from arcsecond.api._constants import API_AUTH_PATH_LOGIN, ARCSECOND_API_URL_DEV
 
 
-@pytest.fixture
-def runner():
-    return CliRunner()
-
-
-def test_cli_basic(runner):
+def test_cli_basic():
+    runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0 and not result.exception
     # Here 'arcsecond' is replaced by 'main' ??
     assert 'Usage: main [OPTIONS] COMMAND [ARGS]' in result.output
 
 
-def test_cli_version(runner):
+def test_cli_version():
+    runner = CliRunner()
     result = runner.invoke(cli.main, ['-V'])
     assert result.exit_code == 0 and not result.exception
     assert re.match('[0-9].[0-9].[0-9]', result.output)
@@ -32,36 +26,30 @@ def test_cli_version(runner):
     assert re.match('[0-9].[0-9].[0-9]', result.output)
 
 
-def test_login_no_server(runner):
+def test_login_no_server():
+    runner = CliRunner()
     result = runner.invoke(cli.login, ['-d'], input='dummy\ndummy')
     assert result.exit_code != 0
     assert isinstance(result.exception, ArcsecondConnectionError)
 
 
 @httpretty.activate
-def test_login_unknown_credentials(runner):
+def test_login_unknown_credentials():
+    runner = CliRunner()
     httpretty.register_uri(
         httpretty.POST,
         ARCSECOND_API_URL_DEV + API_AUTH_PATH_LOGIN,
         status=401,
         body='{"detail": "Unable to log in with provided credentials."}'
     )
-    httpretty.register_uri(
-        httpretty.POST,
-        ARCSECOND_API_URL_PROD + API_AUTH_PATH_LOGIN,
-        status=401,
-        body='{"detail": "Unable to log in with provided credentials."}'
-    )
-    result1 = runner.invoke(cli.login, ['-d'], input='dummy\ndummy')
-    result2 = runner.invoke(cli.login, input='dummy\ndummy')
-    assert result1.exit_code == 0 and not result1.exception
-    assert result2.exit_code == 0 and not result2.exception
-    assert 'Unable to log in with provided credentials.' in result1.output
-    assert 'Unable to log in with provided credentials.' in result2.output
+    result = runner.invoke(cli.login, ['-d'], input='dummy\ndummy')
+    assert result.exit_code == 0 and not result.exception
+    assert 'Unable to log in with provided credentials.' in result.output
 
 
 @httpretty.activate
-def test_login_invalid_parameters(runner):
+def test_login_invalid_parameters():
+    runner = CliRunner()
     httpretty.register_uri(
         httpretty.POST,
         ARCSECOND_API_URL_DEV + API_AUTH_PATH_LOGIN,
