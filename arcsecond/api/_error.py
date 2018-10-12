4d9d4a5be9dd26ecec6b63ef79467fd038c6a25a
Cédric Foellmi <cedric@onekilopars.ec>
2018-10-12 16:28:07 +0200

    Py2.7 compatibility

--- arcsecond/api/_error.py
+++ arcsecond/api/_error.py
@@ -6,11 +6,11 @@ class ArcsecondInvalidEndpointError(ArcsecondError):
     def __init__(self, endpoint, endpoints):
         msg = "Unknown endpoint {}. ".format(endpoint)
         msg += "It must be one of: {}".format(', '.join(endpoints))
-        super().__init__(msg)
+        super(ArcsecondInvalidEndpointError, self).__init__(msg)
 
 
 class ArcsecondConnectionError(ArcsecondError):
     def __init__(self, url):
         msg = "Unable to connect to API server {}.\n".format(url)
         msg += "Suggestion: Test whether it's reachable, by typing for instance: 'ping {}'".format(url)
-        super().__init__(msg)
+        super(ArcsecondConnectionError, self).__init__(msg)
