6f451ba2b545b45c8735e1ab43eca619a1500b18
Cédric Foellmi <cedric@onekilopars.ec>
2019-04-09 18:56:36 +0200

    Returning None explicitely

--- arcsecond/config.py
+++ arcsecond/config.py
@@ -49,7 +49,7 @@ def config_file_read_key(key, debug=False):
     section = 'debug' if debug else 'main'
     if section not in config.sections():
         return None
-    return config[section].get(key)
+    return config[section].get(key, None)
 
 
 def config_file_read_api_key(debug=False):
