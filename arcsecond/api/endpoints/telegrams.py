8b7d1d1e6d0dae15f8cfbd21f9394674d023933c
Cédric Foellmi <cedric@onekilopars.ec>
2019-04-11 12:09:00 +0200

    Consistency

--- arcsecond/api/endpoints/telegrams.py
+++ arcsecond/api/endpoints/telegrams.py
@@ -4,7 +4,7 @@ from ._base import APIEndPoint
 class TelegramsATelAPIEndPoint(APIEndPoint):
     name = 'telegrams_atel'
 
-    def _list_url(self, name=None):
+    def _list_url(self, name=''):
         return self._build_url('telegrams', 'ATel')
 
     def _detail_url(self, identifier):
