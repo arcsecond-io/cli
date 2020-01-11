e919b9b36efe9501025f02c5537034e578e1093d
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-01-11 15:34:43 +0100

    Using debug flag

--- tests/test_arcsecond_root.py
+++ tests/test_arcsecond_root.py
@@ -4,20 +4,20 @@ from .utils import save_test_credentials, clear_test_credentials
 
 def test_default_empty_state():
     clear_test_credentials()
-    assert Arcsecond.is_logged_in(test=True) is False
-    assert Arcsecond.username(test=True) == ''
-    assert Arcsecond.memberships(test=True) == {}
+    assert Arcsecond.is_logged_in(debug=True) is False
+    assert Arcsecond.username(debug=True) == ''
+    assert Arcsecond.memberships(debug=True) == {}
 
 
 def test_default_logged_in_state():
     save_test_credentials('cedric')
-    assert Arcsecond.is_logged_in(test=True) is True
-    assert Arcsecond.username(test=True) == 'cedric'
-    assert Arcsecond.memberships(test=True) == {}
+    assert Arcsecond.is_logged_in(debug=True) is True
+    assert Arcsecond.username(debug=True) == 'cedric'
+    assert Arcsecond.memberships(debug=True) == {}
 
 
 def test_default_logged_in_with_membership_state():
     save_test_credentials('cedric', {'saao': 'superadmin'})
-    assert Arcsecond.is_logged_in(test=True) is True
-    assert Arcsecond.username(test=True) == 'cedric'
-    assert Arcsecond.memberships(test=True) == {'saao': 'superadmin'}
+    assert Arcsecond.is_logged_in(debug=True) is True
+    assert Arcsecond.username(debug=True) == 'cedric'
+    assert Arcsecond.memberships(debug=True) == {'saao': 'superadmin'}
