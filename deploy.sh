c00e2464ad490258f3baaa4cdf6d8901c5b6ac42
Cédric Foellmi <cedric.foellmi@aselta.com>
2022-01-08 15:34:42 +0100

    Adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vitepress/dist
+
+echo 'cli.arcsecond.io' > CNAME
+
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:arcsecond-io/arcsecond-io.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
+
