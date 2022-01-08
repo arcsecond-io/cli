5a3d6321c34d4a1fe609f234c98780b68e4c1bb8
Cédric Foellmi <cedric.foellmi@aselta.com>
2022-01-08 17:45:33 +0100

    Fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -11,14 +11,15 @@ cd docs/.vitepress/dist
 
 echo 'cli.arcsecond.io' > CNAME
 
+git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:arcsecond-io/arcsecond-io.github.io.git master
+# git push -f git@github.com:arcsecond-io/arcsecond-io.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:arcsecond-io/cli.git master:gh-pages
 
 cd -
 
