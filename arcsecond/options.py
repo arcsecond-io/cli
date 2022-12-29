413246ce25c83863c1a0eaf881cb1dcd37e13e1c
Cédric Foellmi <cedric@onekiloparsec.dev>
2022-12-29 23:48:25 +0100

    No more debu and open useless options
    (Making room for a configure method soon..._
    
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- arcsecond/options.py
+++ arcsecond/options.py
@@ -4,34 +4,20 @@ import click
 class State(object):
     def __init__(self,
                  verbose=0,
-                 debug=False,
                  test=False,
-                 open=None,
                  organisation=None,
                  is_using_cli=True,
                  api_key=None,
                  upload_key=None):
-
         self.verbose = verbose
-        self.debug = debug
         self.test = test
-        self.open = open
         self.organisation = organisation
         self.is_using_cli = is_using_cli
         self.api_key = api_key
         self.upload_key = upload_key
 
-    def config_section(self):
-        if self.test:
-            return 'test'
-        elif self.debug:
-            return 'debug'
-        return 'main'
-
     def make_new_silent(self):
         return State(verbose=0,
-                     debug=self.debug,
-                     open=self.open,
                      test=self.test,
                      organisation=self.organisation,
                      is_using_cli=self.is_using_cli,
@@ -67,18 +53,32 @@ def verbose_option_constructor(f):
                         callback=callback)(f)
 
 
-def debug_option_constructor(f):
-    def callback(ctx, param, value):
-        state = ctx.ensure_object(State)
-        state.debug = value
-        return value
-
-    return click.option('-d',
-                        '--debug',
-                        is_flag=True,
-                        expose_value=False,
-                        help='Enables or disables debug mode (for arcsecond developers).',
-                        callback=callback)(f)
+# def debug_option_constructor(f):
+#     def callback(ctx, param, value):
+#         state = ctx.ensure_object(State)
+#         state.debug = value
+#         return value
+#
+#     return click.option('-d',
+#                         '--debug',
+#                         is_flag=True,
+#                         expose_value=False,
+#                         help='Enable debug mode (for Arcsecond developers).',
+#                         callback=callback)(f)
+#
+#
+# def staging_option_constructor(f):
+#     def callback(ctx, param, value):
+#         state = ctx.ensure_object(State)
+#         state.debug = value
+#         return value
+#
+#     return click.option('-s',
+#                         '--staging',
+#                         is_flag=True,
+#                         expose_value=False,
+#                         help='Enable staging mode (for Arcsecond developers).',
+#                         callback=callback)(f)
 
 
 def test_option_constructor(f):
@@ -90,7 +90,7 @@ def test_option_constructor(f):
     return click.option('--test',
                         is_flag=True,
                         expose_value=False,
-                        help='Enables or disables test mode (for arcsecond developers). Implies debug.',
+                        help='Enable test mode (for Arcsecond developers). Implies debug.',
                         callback=callback)(f)
 
 
@@ -105,23 +105,22 @@ def organisation_option_constructor(f):
                         callback=callback)(f)
 
 
-def open_option_constructor(f):
-    def callback(ctx, param, value):
-        state = ctx.ensure_object(State)
-        state.open = value
-        return value
-
-    return click.option('-o',
-                        '--open',
-                        is_flag=True,
-                        expose_value=False,
-                        help="Open the corresponding webpage in the default browser.",
-                        callback=callback)(f)
+# def open_option_constructor(f):
+#     def callback(ctx, param, value):
+#         state = ctx.ensure_object(State)
+#         state.open = value
+#         return value
+#
+#     return click.option('-o',
+#                         '--open',
+#                         is_flag=True,
+#                         expose_value=False,
+#                         help="Open the corresponding webpage in the default browser.",
+#                         callback=callback)(f)
 
 
 def basic_options(f):
     f = verbose_option_constructor(f)
-    f = debug_option_constructor(f)
     f = test_option_constructor(f)
     return f
 
@@ -132,10 +131,10 @@ def organisation_options(f):
     return f
 
 
-def open_options(f):
-    f = basic_options(f)
-    f = open_option_constructor(f)
-    return f
+# def open_options(f):
+#     f = basic_options(f)
+#     f = open_option_constructor(f)
+#     return f
 
 
 class MethodChoiceParamType(click.ParamType):
