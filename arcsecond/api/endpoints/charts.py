412a1ad4dc9b41474ad66816e2518163d8ac1dc9
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-10-04 20:52:24 +0200

    Removed useless endpoints for now
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- arcsecond/api/endpoints/charts.py
+++
@@ -1,9 +0,0 @@
-from ._base import APIEndPoint
-
-
-class FindingChartsAPIEndPoint(APIEndPoint):
-    name = 'findingcharts'
-
-    def _list_url(self, **filters):
-        name = filters.get('name') or ''
-        return self._build_url('findingcharts', name)
--- arcsecond/api/endpoints/objects.py
+++ arcsecond/api/endpoints/objects.py
@@ -7,9 +7,6 @@ class ObjectsAPIEndPoint(APIEndPoint):
     def _detail_url(self, name):
         return self._build_url('objects', name)
 
-    def _open_url(self, name):
-        return self._root_open_url() + '/objects/' + name
-
 
 class ExoplanetsAPIEndPoint(APIEndPoint):
     name = 'exoplanets'
@@ -19,7 +16,3 @@ class ExoplanetsAPIEndPoint(APIEndPoint):
 
     def _detail_url(self, name):
         return self._build_url('exoplanets', name)
-
-    def _open_url(self, name):
-        # Yes, in the web, the url is unified with /objects/.
-        return self._root_open_url() + '/objects/' + name
