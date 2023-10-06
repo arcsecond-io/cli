4fb3fc60c5506d7db5f70b10ac99435e1cb72178
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-10-06 08:23:34 +0200

    Preparing to include beat
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- arcsecond/hosting/constants.py
+++ arcsecond/hosting/constants.py
@@ -4,6 +4,7 @@ DOCKER_IMAGE_CONTAINERS_NAMES = {
     'www': ('arcsecond-www:latest', 'arcsecond-www', 'Webapp'),
     'api': ('arcsecond-api:latest', 'arcsecond-api', 'APIs'),
     'tq': ('arcsecond-api:latest', 'arcsecond-tq', 'Task Queue'),
+    'bt': ('arcsecond-api:latest', 'arcsecond-bt', 'Beat'),
     'db': ('postgres:14', 'arcsecond-db', 'Database'),  # database
     'mb': ('rabbitmq:3.9', 'arcsecond-mb', 'Message Broker'),  # message broker
 }
