798dc069f711bfe1cea25e1162f355041b4fc560
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-12-16 09:56:44 +0100

    Improved arcsecond try
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- arcsecond/hosting/main.py
+++ arcsecond/hosting/main.py
@@ -2,73 +2,59 @@ import sys
 
 import click
 
-from arcsecond import Config
+from arcsecond.hosting import docker
+from arcsecond.hosting import keygen
+from .constants import BANNER, PREFIX
 from .checks import (
-    is_docker_available,
-    setup_docker_host_on_macos,
+    fetch_user_profile,
     is_arcsecond_api_reachable,
     is_user_logged_in,
     has_user_verified_email
 )
-from .constants import DOCKER_IMAGE_CONTAINERS_NAMES, BANNER, PREFIX
-from .containers import (
-    setup_network,
-    run_db_container,
-    run_api_container,
-    run_www_container,
-    run_mb_container
-)
-from .images import (
-    has_all_arcsecond_docker_images,
-    has_docker_image,
-    pull_all_arcsecond_docker_images,
-)
 from .setup import setup_hosting_variables
-from .utils import __get_docker_container_status, __perform_container_bookkeeping
+
+__version__ = '0.1.0 (Alpha) - Please, send feedback to cedric@arcsecond.io'
 
 
 def run_arcsecond(state, do_try=True, skip_setup=False):
     click.echo(BANNER)
+    click.echo('\n' + PREFIX + __version__)
     click.echo("\n" + PREFIX + "Checking prerequisites...")
-    if not is_docker_available():
+    if not docker.is_docker_available():
         return
     if sys.platform == 'darwin':
-        setup_docker_host_on_macos()
-    if not is_arcsecond_api_reachable():
+        docker.setup_docker_host_on_macos()
+    if not is_arcsecond_api_reachable(state):
         return
     if not is_user_logged_in(state):
         return
-    config = Config(state)
     if do_try is False and not has_user_verified_email(state):
         return
-    # LICENSING...
+
+    # config = Config(state)
+    klient = keygen.KeygenClient(state, do_try)
+    profile = fetch_user_profile(state)
+    klient.create_user(profile)
+    klient.create_license()
+
+    return
+
     if not skip_setup:
         setup_hosting_variables(config, do_try=do_try)
-    if not has_all_arcsecond_docker_images():
-        pull_all_arcsecond_docker_images()
-    setup_network()
-    run_db_container(restart=False)
-    run_mb_container(restart=False)
-    run_api_container(config, restart=False, do_try=do_try)
-    run_www_container(restart=True)
+    if not docker.has_all_arcsecond_docker_images():
+        docker.pull_all_arcsecond_docker_images()
+    docker.setup_network()
+    docker.run_db_container(restart=False)
+    docker.run_mb_container(restart=False)
+    docker.run_api_container(config, restart=False, do_try=do_try)
+    docker.run_www_container(restart=True)
 
 
 def stop_arcsecond():
     if sys.platform == 'darwin':
-        setup_docker_host_on_macos()
-    container_names = [cont for (_, cont, _) in DOCKER_IMAGE_CONTAINERS_NAMES.values()]
-    for container_name in container_names:
-        __perform_container_bookkeeping(container_name, stop=True)
+        docker.setup_docker_host_on_macos()
+    docker.stop_all_containers()
 
 
-def get_arcsecond_status():
-    if sys.platform == 'darwin':
-        setup_docker_host_on_macos()
-    for image_name, container_name, service_name in DOCKER_IMAGE_CONTAINERS_NAMES.values():
-        container_status = __get_docker_container_status(container_name)
-        container_status_msg = f'Container status: {container_status}.' \
-            if container_status is not None \
-            else 'No running container.'
-        image_status = has_docker_image(image_name)
-        image_status_msg = 'Docker image locally available.' if image_status is True else 'Image not yet pulled.'
-        click.echo(f'Service "{service_name}": {image_status_msg} {container_status_msg}')
+def print_arcsecond_status():
+    click.echo(docker.get_all_containers_status_string())
