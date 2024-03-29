9f1c8e7a99289ffead5aafd3fe88e6284040195a
Cédric Foellmi <cedric@onekiloparsec.dev>
2024-03-29 12:08:00 +0100

    Makine tests of login work
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/module/test_login.py
+++ tests/module/test_login.py
@@ -1,45 +1,58 @@
 import httpretty
 
-from arcsecond import ArcsecondAPI, config
-from tests.utils import TEST_API_KEY, TEST_LOGIN_PASSWORD, TEST_LOGIN_USERNAME, TEST_UPLOAD_KEY, clear_test_credentials, \
+from arcsecond import ArcsecondAPI, ArcsecondConfig
+from arcsecond.api.constants import ARCSECOND_API_URL_DEV
+from arcsecond.options import State
+from tests.utils import (
+    TEST_API_KEY,
+    TEST_LOGIN_PASSWORD,
+    TEST_LOGIN_USERNAME,
+    TEST_UPLOAD_KEY,
+    clear_test_credentials,
     prepare_successful_login
+)
 
 
 @httpretty.activate
 def test_login_basic():
     clear_test_credentials()
-    assert config.config_file_read_api_key('test') is None
+    config = ArcsecondConfig(State(api_name='test'))
+    config.api_server = ARCSECOND_API_URL_DEV
+    assert config.access_key == ''
     prepare_successful_login()
-    ArcsecondAPI.login(TEST_LOGIN_USERNAME, TEST_LOGIN_PASSWORD, api='test')
-    assert config.config_file_read_api_key('test') is None
-    assert config.config_file_read_upload_key('test') is None
+    ArcsecondAPI(config).login(TEST_LOGIN_USERNAME, TEST_LOGIN_PASSWORD)
+    assert config.access_key == ''
+    assert config.upload_key == ''
 
 
 @httpretty.activate
-def test_login_apikey():
+def test_login_acess_key():
     clear_test_credentials()
-    assert config.config_file_read_api_key('test') is None
+    config = ArcsecondConfig(State(api_name='test'))
+    config.api_server = ARCSECOND_API_URL_DEV
     prepare_successful_login()
-    ArcsecondAPI.login(TEST_LOGIN_USERNAME, TEST_LOGIN_PASSWORD, access_key=True, api='test')
-    assert config.config_file_read_api_key('test') == TEST_API_KEY
-    assert config.config_file_read_upload_key('test') is None
+    ArcsecondAPI(config).login(TEST_LOGIN_USERNAME, TEST_LOGIN_PASSWORD, access_key=True)
+    assert config.access_key == TEST_API_KEY
+    assert config.upload_key == ''
 
 
 @httpretty.activate
-def test_login_uploadkey():
+def test_login_upload_key():
     clear_test_credentials()
-    assert config.config_file_read_api_key('test') is None
+    config = ArcsecondConfig(State(api_name='test'))
+    config.api_server = ARCSECOND_API_URL_DEV
     prepare_successful_login()
-    ArcsecondAPI.login(TEST_LOGIN_USERNAME, TEST_LOGIN_PASSWORD, upload_key=True, api='test')
-    assert config.config_file_read_api_key('test') is None
-    assert config.config_file_read_upload_key('test') == TEST_UPLOAD_KEY
+    ArcsecondAPI(config).login(TEST_LOGIN_USERNAME, TEST_LOGIN_PASSWORD, upload_key=True)
+    assert config.access_key == ''
+    assert config.upload_key == TEST_UPLOAD_KEY
 
 
 @httpretty.activate
-def test_login_both_apikey_uploadkey():
+def test_login_both_access_key_and_upload_key():
     clear_test_credentials()
-    assert config.config_file_read_api_key('test') is None
+    config = ArcsecondConfig(State(api_name='test'))
+    config.api_server = ARCSECOND_API_URL_DEV
     prepare_successful_login()
-    ArcsecondAPI.login(TEST_LOGIN_USERNAME, TEST_LOGIN_PASSWORD, access_key=True, upload_key=True, api='test')
-    assert config.config_file_read_api_key('test') == TEST_API_KEY
-    assert config.config_file_read_upload_key('test') == TEST_UPLOAD_KEY
+    ArcsecondAPI(config).login(TEST_LOGIN_USERNAME, TEST_LOGIN_PASSWORD, access_key=True, upload_key=True)
+    assert config.access_key == TEST_API_KEY
+    assert config.upload_key == TEST_UPLOAD_KEY
