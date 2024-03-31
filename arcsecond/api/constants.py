1cc554cdfdc33591f75d3fd9700125621a950218
Cédric Foellmi <cedric@onekiloparsec.dev>
2024-03-31 20:15:14 +0530

    Fixed the auth paths
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- arcsecond/api/constants.py
+++ arcsecond/api/constants.py
@@ -4,5 +4,5 @@ ARCSECOND_API_URL_PROD = 'https://api.arcsecond.io'
 ARCSECOND_WWW_URL_DEV = 'http://localhost:8080'
 ARCSECOND_WWW_URL_PROD = 'https://www.arcsecond.io'
 
-API_AUTH_PATH_VERIFY = '/auth/key/verify/'
-API_AUTH_PATH_VERIFY_PORTAL = '/auth/key/verify-organisation/'
+API_AUTH_PATH_VERIFY = 'auth/key/verify'
+API_AUTH_PATH_VERIFY_PORTAL = 'auth/key/verify-organisation'
