55cd73ee945ae08db963dc0736128833d29f467c
Cédric Foellmi <cedric@onekiloparsec.dev>
2024-04-22 08:30:45 +0200

    New errors for telescope
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- arcsecond/uploader/errors.py
+++ arcsecond/uploader/errors.py
@@ -71,9 +71,31 @@ class InvalidDatasetError(ArcsecondError):
         super().__init__(msg)
 
 
+class InvalidTelescopeError(ArcsecondError):
+    def __init__(self, telescope_uuid, error_string=''):
+        msg = f'Invalid / unknown telescope with UUID {telescope_uuid}.'
+        if error_string:
+            msg += f'\n{error_string}'
+        super().__init__(msg)
+
+
+class InvalidTelescopeInDatasetError(ArcsecondError):
+    def __init__(self, telescope_uuid, dataset_telescope_uuid, dataset_uuid):
+        msg = f'Telescope {telescope_uuid} does not match with existing {dataset_telescope_uuid} in Dataset {dataset_uuid}.'
+        super().__init__(msg)
+
+
 class InvalidOrganisationDatasetError(ArcsecondError):
     def __init__(self, dataset_uuid, org_subdomain, error_string=''):
         msg = f'Dataset with UUID {dataset_uuid} unknown within organisation {org_subdomain}.'
+        if error_string:
+            msg += f'\n{error_string}'
+        super().__init__(msg)
+
+
+class InvalidOrganisationTelescopeError(ArcsecondError):
+    def __init__(self, telescope_uuid, org_subdomain, error_string=''):
+        msg = f'Telescope with UUID {telescope_uuid} unknown within organisation {org_subdomain}.'
         if error_string:
             msg += f'\n{error_string}'
         super().__init__(msg)
