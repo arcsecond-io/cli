4c97d39c09a9b01afabd62dcf60f29dd37c9de31
Cédric Foellmi <cedric@onekiloparsec.dev>
2025-06-12 17:46:20 +0200

    Commenting out EMAIL settings for now
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- arcsecond/hosting/setup.py
+++ arcsecond/hosting/setup.py
@@ -66,55 +66,55 @@ def setup_hosting_variables(config: ArcsecondConfig, do_try=True):
     click.echo("Note that you will need a recent version of Docker running on this machine for Arcsecond to run.")
     click.echo("A connexion to Internet is also required.")
 
-    click.echo("\nPlease answer the following questions to setup your local installation.")
-    click.echo(f"Answers will be stored in {str(config.file_path)}. Keep this file safely and private.")
-    click.echo("To update an installation, simply re-run the \"try\" command.")
-    if do_try:
-        click.echo("A valid Arcsecond.io account is required for trying Arcsecond.")
-        click.echo("Please, use the command `arcsecond login` or `arcsecond register` if needed.")
-    else:
-        # check credentials
-        pass
-
-    click.echo("")
-    click.echo("If you intend to create and use an Observatory Portal, Arcsecond requires a subdomain.")
-    subdomain = click.prompt("Choose an organisation subdomain for your portal",
-                             value_proc=_validate_subdomain)
-    config.save(org_subdomain=subdomain, section=section)
-
-    # port = click.prompt("Choose an address port number for your API server", type=click.INT, default='8000')
-    # config_file_save_key_value('api_port', port, section=section)
-    current_value = config.read_key('email_host', section)
-    msg = "Provide the email host address. Start your answer with $ to provide an env variable name."
-    email_host = click.prompt(msg,
-                              value_proc=__validate_email_host,
-                              default=current_value or '$EMAIL_HOST')
-    config.save(email_host=email_host, section=section)
-
-    msg = "Provide the email host username. Start your answer with $ to provide an env variable name."
-    current_value = config.read_key('email_host_user', section)
-    email_host_user = click.prompt(msg,
-                                   value_proc=_validate_email_host_item,
-                                   default=current_value or '$EMAIL_HOST_USER')
-    config.save(email_host_user=email_host_user, section=section)
-
-    msg = "Provide the email host password. Start your answer with $ to provide an env variable name."
-    current_value = config.read_key('email_host_password', section)
-    email_host_password = click.prompt(msg,
-                                       value_proc=_validate_email_host_item,
-                                       default=current_value or '$EMAIL_HOST_PASSWORD')
-    config.save(email_host_password=email_host_password, section=section)
-
-    msg = "Provide the email address of the administrator. Start your answer with $ to provide an env variable name."
-    current_value = config.read_key('email_admin', section)
-    email_admin = click.prompt(msg,
-                               value_proc=_validate_email_admin,
-                               default=current_value or '$EMAIL_ADMIN')
-    config.save(email_admin=email_admin, section=section)
-
-    msg = "Provide the path of the default storage space. "
-    msg += "Arcsecond requires anyway some storage for images and database.\n"
-    msg += "Later, you will be able to provide external storages too in the running Arcsecond itself."
-    current_value = config.read_key('media_root', section)
-    media_root = click.prompt(msg, type=click.STRING, default=current_value or '$HOME/arcsecond')
-    config.save(media_root=media_root, section=section)
+    # click.echo("\nPlease answer the following questions to setup your local installation.")
+    # click.echo(f"Answers will be stored in {str(config.file_path)}. Keep this file safely and private.")
+    # click.echo("To update an installation, simply re-run the \"try\" command.")
+    # if do_try:
+    #     click.echo("A valid Arcsecond.io account is required for trying Arcsecond.")
+    #     click.echo("Please, use the command `arcsecond login` or `arcsecond register` if needed.")
+    # else:
+    #     # check credentials
+    #     pass
+    #
+    # click.echo("")
+    # click.echo("If you intend to create and use an Observatory Portal, Arcsecond requires a subdomain.")
+    # subdomain = click.prompt("Choose an organisation subdomain for your portal",
+    #                          value_proc=_validate_subdomain)
+    # config.save(org_subdomain=subdomain)
+    #
+    # # port = click.prompt("Choose an address port number for your API server", type=click.INT, default='8000')
+    # # config_file_save_key_value('api_port', port)
+    # current_value = config.read_key('email_host')
+    # msg = "Provide the email host address. Start your answer with $ to provide an env variable name."
+    # email_host = click.prompt(msg,
+    #                           value_proc=__validate_email_host,
+    #                           default=current_value or '$EMAIL_HOST')
+    # config.save(email_host=email_host)
+    #
+    # msg = "Provide the email host username. Start your answer with $ to provide an env variable name."
+    # current_value = config.read_key('email_host_user')
+    # email_host_user = click.prompt(msg,
+    #                                value_proc=_validate_email_host_item,
+    #                                default=current_value or '$EMAIL_HOST_USER')
+    # config.save(email_host_user=email_host_user)
+    #
+    # msg = "Provide the email host password. Start your answer with $ to provide an env variable name."
+    # current_value = config.read_key('email_host_password')
+    # email_host_password = click.prompt(msg,
+    #                                    value_proc=_validate_email_host_item,
+    #                                    default=current_value or '$EMAIL_HOST_PASSWORD')
+    # config.save(email_host_password=email_host_password)
+    #
+    # msg = "Provide the email address of the administrator. Start your answer with $ to provide an env variable name."
+    # current_value = config.read_key('email_admin')
+    # email_admin = click.prompt(msg,
+    #                            value_proc=_validate_email_admin,
+    #                            default=current_value or '$EMAIL_ADMIN')
+    # config.save(email_admin=email_admin)
+    #
+    # msg = "Provide the path of the default storage space. "
+    # msg += "Arcsecond requires anyway some storage for images and database.\n"
+    # msg += "Later, you will be able to provide external storages too in the running Arcsecond itself."
+    # current_value = config.read_key('media_root')
+    # media_root = click.prompt(msg, type=click.STRING, default=current_value or '$HOME/arcsecond')
+    # config.save(media_root=media_root)
