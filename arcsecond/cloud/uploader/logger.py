27604eaf362ed9b7893579da6af8aa9e14362170
Cédric Foellmi <cedric@onekiloparsec.dev>
2025-07-08 10:37:04 +0200

    No log files for now
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- arcsecond/cloud/uploader/logger.py
+++ arcsecond/cloud/uploader/logger.py
@@ -12,12 +12,12 @@ def get_logger(debug=False) -> Logger:
     if len(logger.handlers) == 0:
         formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-        if os.environ.get('OORT_TESTS') != '1':
-            log_file_path = Path(f'arcsecond_upload_{datetime.now().replace(microsecond=0).isoformat()}.log')
-            file_handler = FileHandler(log_file_path)
-            file_handler.setLevel(DEBUG if debug else INFO)
-            file_handler.setFormatter(formatter)
-            logger.addHandler(file_handler)
+        # if os.environ.get('OORT_TESTS') != '1':
+        #     log_file_path = Path(f'arcsecond_upload_{datetime.now().replace(microsecond=0).isoformat()}.log')
+        #     file_handler = FileHandler(log_file_path)
+        #     file_handler.setLevel(DEBUG if debug else INFO)
+        #     file_handler.setFormatter(formatter)
+        #     logger.addHandler(file_handler)
 
         console_handler = StreamHandler()
         console_handler.setLevel(DEBUG if debug else INFO)
